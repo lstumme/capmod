b8ba77f00ed9f81843247bf67842b969e76d5580
Ludovic Stumme <ludovic.stumme@gmail.com>
2021-07-06 09:45:42 +0200

    Affichage de la commande dans le diagramme

---
+++ com.navalgroup.capella.modelicalink/src/com/navalgroup/capella/modelicalink/handlers/ExportToModelicaHandler.java
@@ -0,0 +1,16 @@
+package com.navalgroup.capella.modelicalink.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ExportToModelicaHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
